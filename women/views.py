5643b153b9e17afc4636f902af59eab32c6bb695
dotacry <dota2cry@gmail.com>
2024-03-06 13:37:06 +0300

    selfedudrf4

---
+++ women/views.py
@@ -0,0 +1,45 @@
+from django.forms import model_to_dict
+from rest_framework import generics
+from django.shortcuts import render
+from .models import Women
+from .serializers import WomenSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+# Create your views here.
+#class WomenAPIView(generics.ListAPIView):
+#    queryset = Women.objects.all()
+#    serializer_class = WomenSerializer
+
+# class WomenAPIView(APIView):
+#     def get(self, request):
+#         lst = Women.objects.all().values()
+#         return Response({'posts': list(lst)})
+#
+#     def post(self, request):
+#         post_new = Women.objects.create(
+#             title=request.data['title'],
+#             content=request.data['content'],
+#             cat_id=request.data['cat_id']
+#         )
+#
+#         return Response({'post': model_to_dict(post_new)})
+
+
+class WomenAPIView(APIView):
+    def get(self, request):
+        w = Women.objects.all()
+        return Response({'posts': WomenSerializer(w, many=True).data})
+
+    def post(self, request):
+        serializer = WomenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        post_new = Women.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id']
+        )
+
+        return Response({'post': WomenSerializer(post_new).data})
