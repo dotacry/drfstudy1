4a9518e0233fa358ba88417c8d57738b683dd366
moycomp <serfonix228@gmail.com>
2024-03-17 00:47:45 +0300

    drf10

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -17,16 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 from women.views import *
-from rest_framework import routers
-
-
-router = routers.SimpleRouter()
-router.register(r'women', WomenViewSet)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls)),  # http://127.0.0.1:8000/api/v1/women/
-    # path('api/v1/womenlist/', WomenViewSet.as_view({'get': 'list'})),
-    # path('api/v1/womenlist/<int:pk>/', WomenViewSet.as_view({'put': 'update'})),
+    path('api/v1/women/', WomenAPIList.as_view()),
+    path('api/v1/women/<int:pk>/', WomenAPIUpdate.as_view()),
+    path('api/v1/womendelete/<int:pk>/', WomenAPIDestroy.as_view()),
 ]
